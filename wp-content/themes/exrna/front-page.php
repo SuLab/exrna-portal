6de164fa286ffb2ae037c276768c91db0b300dde
Iamjoshing <josh@pullmkt.com>
2014-02-04 16:54:10 -0800

    Close Modal Stops Video

--- wp-content/themes/exrna/front-page.php
+++ wp-content/themes/exrna/front-page.php
@@ -130,7 +130,7 @@
 $('body').addClass('home');
 $('body').attr('id', 'page');
 $( '#exrnalogo' ).replaceWith('<a class="navbar-brand visible-lg visible-md visible-sm" href="/" title="exRNA" rel="home"><img src="./media/exrna-logo-white.svg" alt="exRNA" class="logo img-responsive" onerror="this.onerror=null; this.src="./media/exrna-logo-white.png"></a><a class="navbar-brand hidden-lg hidden-md hidden-sm visible-xs navbar-text-logo" href="/" title="exRNA" rel="home"><img src="./media/exrna-logo-white-small.svg" alt="exRNA" class="logo img-responsive" onerror="this.onerror=null; this.src="./media/exrna-logo-white-small.png"></a>');
-	
+$('.modal').each(function(){var src = $(this).find('iframe').attr('src');$(this).on('click', function(){$(this).find('iframe').attr('src', '');$(this).find('iframe').attr('src', src);});});
 </script>
 
     <!-- BigVideo Dependencies -->
