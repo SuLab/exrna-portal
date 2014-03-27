f8e339869b7211bbc77c1895e6254529181e2eff
Iamjoshing <josh@pullmkt.com>
2014-03-27 15:12:27 -0700

    Began Projects Page Layout

---
+++ wp-content/themes/exrna/projects-template.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: Projects 2
+*/
+?>
+
+<?php get_header(); ?>
+<div class="container pushtop">
+	<h1>Projects 2</h1>
+	<div class="row pushtop">
+		<div class="col-sm-12 project-box">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-3">
+							<img src="http://placehold.it/90x90" alt="" class="img-responsive img-rounded pull-left project-header-img ">
+						</div>
+						<div class="col-md-9">
+							<p>(u54) rfa-rm-12-010</p>
+		                    <h4><a href="http://localhost:8888/exrnawp/dmrr/" title="data management and resource repository (dmrr) on extracellular rna (u54) rfa-rm-12-010 ">Data Management And Resource Repository (DMRR) On Extracellular RNA</a></h4>
+						</div>
+					</div>
+				</div>
+			<hr>
+			</div>
+			
+			<div class="row pushtop">
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-3">
+							<img src="http://placehold.it/90x90" alt="" class="img-responsive img-rounded pull-left project-header-img ">
+						</div>
+						<div class="col-md-9">
+							<p><a href="http://localhost:8888/exrnawp/projects/data-management-resource-repository-exrna-atlas/">Data Management and Resource Repository for the exRNA Atlas</a></p>
+	                        <p>Aleksandar Milosavljevic</p>
+	                        <p>Baylor College of Medicine</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-3">
+							<img src="http://placehold.it/90x90" alt="" class="img-responsive img-rounded pull-left project-header-img ">
+						</div>
+						<div class="col-md-9">
+							<p><a href="http://localhost:8888/exrnawp/projects/data-management-resource-repository-exrna-atlas/">Data Management and Resource Repository for the exRNA Atlas</a></p>
+	                        <p>Aleksandar Milosavljevic</p>
+	                        <p>Baylor College of Medicine</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div> 
+
+
+
+
+<?php get_footer(); ?>
