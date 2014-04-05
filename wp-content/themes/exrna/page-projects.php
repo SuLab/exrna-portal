9138481f0a495b55f80622f9218cff37f86545b6
Iamjoshing <josh@pullmkt.com>
2014-04-05 10:48:28 -0700

    removing images for projects

--- wp-content/themes/exrna/page-projects.php
+++ wp-content/themes/exrna/page-projects.php
@@ -36,7 +36,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/vivo-regulated-release-function-extracellular-small-rnas/">In Vivo Regulated Release and Function Of Extracellular Small RNAs <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Robert H. Blelloch</h5>
 		                        <h6>University of California, San Francisco</td>
@@ -48,7 +48,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/exrna-released-glioblastoma-alters-brain-microenvironment/">Exrna Released By Glioblastoma Alters Brain Microenvironment <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Xandra Owens Breakefield</h5>
 		                        <h6>Massachusetts General Hospital</h6>
@@ -63,7 +63,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/secreted-rna-crc-progression-biogenesis-function-clinical-markers/">Secreted RNA During CRC Progression Biogenesis Function And Clinical Markers <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Robert J. Coffey</h5>
 		                        <h6>Vanderbilt University Medical Center</h6>
@@ -75,7 +75,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/genetic-models-exrna-communication/">Genetic Models for exRNA Communication <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Michael T. Mcmanus</h5>
 		                        <h6>University of California, San Francisco</h6>
@@ -90,7 +90,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/definition-serum-ribonucleoprotein-composition-regulation-function/">Definition Of Serum Ribonucleoprotein Composition And Its Regulation And Function <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Thomas Tuschl</h5>
 		                        <h6>rockefeller university</h6>
@@ -127,7 +127,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/exrna-biomarkers-human-glioma/" title="exrna biomarkers for human glioma">exRNA Biomarkers for Human Glioma <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Bob S. Carter</h5>
 		                        <h6>University Of California, San Diego</h6>
@@ -139,7 +139,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/plasma-mirna-predictors-adverse-mechanical-electrical-remodeling-mi/" title="plasma mirna predictors of adverse mechanical and electrical remodeling after mi">Plasma miRNA Predictors Of Adverse Mechanical And Electrical Remodeling After mi <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Saumya Das</h5>
 		                        <h6>Beth Israel Deaconess Medical Center</h6>
@@ -154,7 +154,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/extracellular-rnas-biomarkers-cardiovascular-risk-disease/" title="extracellular rnas: biomarkers for cardiovascular risk and disease">extracellular RNAs: biomarkers for cardiovascular risk and disease <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Jane E. Freedman</h5>
 		                        <h6>University of Massachusetts Medical School, Worcester</h6>
@@ -166,7 +166,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/exrna-signatures-predict-outcomes-brain-injury/" title="exrna signatures predict outcomes after brain injury">exRNA Signatures Predict Outcomes After Brain Injury <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Matthew Huentelman</h5>
 		                        <h6>Translational Genomics Research Institute</h6>
@@ -181,7 +181,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/exrnas-early-identification-pregnancies-risk-placental-dysfunction/" title="exrnas for early identification of pregnancies at risk for placental dysfunction">exRNAs For Early Identification Of Pregnancies At Risk For Placental Dysfunction <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Louise C. Laurent</h5>
 		                        <h6>University Of California, San Diego</h6>
@@ -193,7 +193,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/extracellular-non-coding-rna-biomarkers-hepatocellular-cancer/" title="extracellular non-coding rna biomarkers of hepatocellular cancer">eExtracellular Non-coding RNA Biomarkers Of Hepatocellular Cancer <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Tushar Patel</h5>
 		                        <h6>Mayo Clinic, Jacksonville</h6>
@@ -208,7 +208,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/clinical-utility-micrornas-diagnostic-biomarkers-alzheimers-disease/" title="clinical utility of micrornas as diagnostic biomarkers of alzheimers disease">Clinical Utility Of MicroRNAs As Diagnostic Biomarkers Of Alzheimer's Disease <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Julie Anne Saugstad</h5>
 		                        <h6>Oregon Health &amp; Science University</h6>
@@ -220,7 +220,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/clinical-utility-extracellular-rna-marker-kidney-disease-progression/" title="clinical utility of extracellular rna as marker of kidney disease progression">Clinical Utility Of Extracellular RNA As Marker Of Kidney Disease Progression <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 	                          	<h5>Thomas Tuschl</h5>
 	                          	<h6>Rockefeller University</h6>
@@ -235,7 +235,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/circulating-micrornas-disease-biomarkers-multiple-sclerosis/" title="circulating micrornas as disease biomarkers in multiple sclerosis">Circulating MicroRNAs As Disease Biomarkers In Multiple Sclerosis <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Howard L. Weiner</h5>
 		                        <h6>Brigham And Women's Hospital</h6>
@@ -247,7 +247,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/clinical-utility-salivary-exrna-biomarkers-gastric-cancer-detection/" title="clinical utility of salivary exrna biomarkers for gastric cancer detection">Clinical Utility Of Salivary exRNA Biomarkers For Gastric Cancer Detection <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>David T. Wong</h5>
 		                        <h6>University Of California, Los Angeles</h6>
@@ -283,7 +283,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/targeting-tumor-derived-exrna-containing-microvesicles-high-throughput-screeni/" title="targeting tumor-derived exrna-containing microvesicles by high throughput screeni">Targeting Tumor-derived exRNA-containing Microvesicles By High Throughput Screeni <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Asim B. Abdel-Mageed</h5>
 					            <h6>Tulane University Of Louisiana</h6>
@@ -295,7 +295,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/exosome-based-therapeutics-huntingtons-disease/" title="exosome based therapeutics in huntingtons disease">Exosome Based Therapeutics In Huntington's Disease <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Neil Aronin</h5>
 					            <h6>University of Massachusetts Medical School, Worcester</h6>
@@ -310,7 +310,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/exosome-rna-therapeutics-promote-cna-myelination/" title="exosome rna-therapeutics to promote cns myelination">Exosome RNA-therapeutics To Promote CNA Myelination <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Richard P. Kraig</h5>
 					            <h6>University Of Chicago</h6>
@@ -322,7 +322,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/her2-targeted-exosomal-delivery-therapeutic-mrna-enzyme-pro-drug-therapy/" title="her2-targeted exosomal delivery of therapeutic mrna for enzyme pro-drug therapy">HER2-targeted Exosomal Delivery Of Therapeutic mRNA For Enzyme Pro-drug Therapy <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>AC Matin</h5>
 					            <h6>Stanford University</h6>
@@ -337,7 +337,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/regulation-of-renal-and-bone-marrow-injury-by-extracellular-vesicle-non-coding-rn/" title="regulation of renal and bone marrow injury by extracellular vesicle non-coding rn">Regulation Of Renal And Bone Marrow Injury By Extracellular Vesicle Non-coding RN <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Peter J. Quesenberry</h5>
 					            <h6>Rhode Island Hospital</h6>
@@ -349,7 +349,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/targeted-delivery-microrna-loaded-microvesicle-cancer-therapy/" title="targeted delivery of microrna-loaded microvesicle for cancer therapy">Targeted Delivery Of MicroRNA-loaded Microvesicle For Cancer Therapy <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Thomas D. Schmittgen</h5>
 					            <h6>Ohio State University</h6>
@@ -364,7 +364,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/novel-extra-cellular-rna-based-combinatorial-rna-inhibition-therapy/" title="novel extra cellular rna-based combinatorial rna inhibition therapy">Novel Extra Cellular RNA-based Combinatorial RNA Inhibition Therapy <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Anil K. Sood</h5>
 					            <h6>UT MD Anderson Cancer Center</h6>
@@ -376,7 +376,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/fruit-exosome-like-particles-therapeutic-delivery-extracellular-mirnas/" title="fruit exosome-like particles for therapeutic delivery of extracellular mirnas">Fruit Exosome-like Particles For Therapeutic Delivery Of Extracellular miRNAs <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 					            <h5>Huang-ge Zhang</h5>
 					            <h6>University Of Louisville</h6>
@@ -413,7 +413,7 @@ Template Name: Project 2 Page
 							<div class="col-3">
 								<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
 							</div>
-							<div class="col-md-9 col-sm-10 col-xs-8">
+							<div class="col-md-9 col-sm-10 col-xs-12">
 								<h4><a href="<?php bloginfo( 'url' ); ?>/projects/data-management-resource-repository-exrna-atlas/">Data Management and Resource Repository for the exRNA Atlas <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
 		                        <h5>Aleksandar Milosavljevic</h5>
 		                        <h6>Baylor College of Medicine</h6>
