5e51327846a4dcd4709f38620790b79ac1c623db
Iamjoshing <josh@pullmkt.com>
2014-04-16 13:49:36 -0700

    projects page toggle comments and remove VIEW NIH

--- wp-content/themes/exrna/single-projects.php
+++ wp-content/themes/exrna/single-projects.php
@@ -9,7 +9,7 @@ get_header(); ?>
 <div class="container pushtop sp">
 	<div class="row">
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<div class="col-md-12 hm-post">
+			<div class="col-md-10 hm-post">
 				<div class="row">
 					<div class="heading">
 						<p><span class="post-date"><?php the_field('project_number'); ?> &bull; <?php the_field('awardee_organization'); ?> &bull; <?php the_time('m.d.y') ?></p>
@@ -20,7 +20,10 @@ get_header(); ?>
 		</article>
 	</div><!-- End Row -->
 	<div class="row">
-		<div class="col-md-7 col-md-offset-2">
+		<div class="col-md-3">
+			<img src="http://placehold.it/300x300" alt="" class="project-main-img img-responsive">
+		</div>
+		<div class="col-md-7 col-md-offset-">
 			<div class="row">
 				<div class="col-xs-4"><div class="hm-post-border"></div></div>
 			</div>
@@ -31,9 +34,7 @@ get_header(); ?>
 					<p><span class="projectlead">Principal Investigator:</span> <?php the_field('contact_pi_project_leader'); ?></p>
 					<p><span class="projectlead">Contributors:</span> <?php the_field('contributors'); ?></p>
 					</div>
-					<div class="col-md-4">
-						<a href="<?php the_field('nih_link'); ?>"><button class="btn btn-teal"><span class="post-date">VIEW NIH</span></button></a>
-					</div>
+					
 				</div>
 				<div class="row">
 					<div class="col-md-12 project-content">
@@ -54,18 +55,60 @@ get_header(); ?>
 				<p><?php previous_post('<i class="fa fa-long-arrow-left"></i> %', '', 'yes'); ?><br></p>
 				<p><?php next_post('%  <i class="fa fa-long-arrow-right"></i> ', '', 'yes'); ?></p>
 			</div> -->
-			<div class="sp-comments">
-				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template();
-				?>
+				<div class="sp-comments">
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php endwhile; // end of the loop. ?>
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || '0' != get_comments_number() )
+							comments_template();
+					?>
+
+					<?php endwhile; // end of the loop. ?>
+				</div>
+				
+			<div class="row pushtop">
+				<div class="col-xs-12">
+					<div class="row">
+						<div class="liner pushbot"></div>
+					</div>
+					
+					<p class="comments">Related Projects</p>
+
+					
+					<div class="project-box funded-projects">
+						<?php 
+ 
+						$posts = get_field('relatation');
+						 
+						if( $posts ): ?>
+						    
+						    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+						        <?php setup_postdata($post); ?>
+						        
+									<div class="row the-projects pushbot">
+										<div class="col-3">
+											<img src="http://placehold.it/90x90" alt="" class="img-responsive project-header-img ">
+										</div>
+										<div class="col-md-9 col-sm-10 col-xs-12">
+											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?> <span class="l-more"><i class="fa fa-long-arrow-right"></i></span></a></h4>
+					                        <h5><?php the_field('contact_pi_project_leader'); ?></h5>
+					                        <h6><?php the_field('awardee_organization'); ?></h6>
+					                        </div>
+										</div>
+									
+						    <?php endforeach; ?>
+						    
+						    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+						<?php endif; ?>
+					</div>
+		
+				</div>
 			</div>
 		</div>
+
+		
 		
 		<!-- <div class="col-md-4">
 			<div class="row sp-sidebar">
@@ -111,8 +154,7 @@ get_header(); ?>
 	</div><!-- End Row -->
 </div><!-- End Container -->
 
-	
-	
+
 
 
 <?php get_footer(); ?>
