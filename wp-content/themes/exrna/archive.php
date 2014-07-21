dbfe9ebfdcb704a311ac261a8839271adfe5f60a
Iamjoshing <josh@pullmkt.com>
2014-07-21 10:51:26 -0700

    archives.php template update

--- wp-content/themes/exrna/archive.php
+++ wp-content/themes/exrna/archive.php
@@ -13,13 +13,13 @@
 get_header(); ?>
 <div class="container pushtop">
 	<div class="row pushtop hm-posts">
-		<div class="col-md-5 col-md-offset-1">
+		<div class="col-md-8">
 			<?php // add the class "panel" below here to wrap the content-padder in Bootstrap style ;) ?>	
 	<div class="content-padder">
 		
 		<?php if ( have_posts() ) : ?>
 	
-			<header class="page-header">
+			<header class="">
 				<h1 class="page-title">
 					<?php
 						if ( is_category() ) :
@@ -87,7 +87,7 @@ get_header(); ?>
 					 * If you want to overload this in a child theme then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					get_template_part( 'content-blog', get_post_format() );
 				?>
 	
 			<?php endwhile; ?>
